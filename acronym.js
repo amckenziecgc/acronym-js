d6674fc31a8ccfa262f8651d818dfa333884df70
Andrew McKenzie <andrew.mckenzie@grainscanada.gc.ca>
2024-02-05 14:26:57 -0600

    slow acronyms

--- acronym.js
+++ acronym.js
@@ -1,1 +1,24 @@
-export function parse() {}
+export function parse(input) {
+    //validate it's a string
+    //let's pretend
+    
+    //everything is a regex...
+    const delimit = /\s/
+
+    //got stuck trying to debug regexes....
+    //const delimit = /\s+.*-/;
+
+    //break the input into a sectioned array
+    const words = input.split(delimit)
+
+    //flatten the sections into one capitalized return string
+    const letters = words.map(grabFirstChar)
+    const output = letters.join('')
+
+    return output
+}
+
+//there's a functional way to to do this, surely?
+function grabFirstChar(input){
+    return input.charAt(0).toUpperCase()
+}
